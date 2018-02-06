8b59b38947f33dc37eba1266331afef9d60fc9f7
Devin Yin <devinyin@augmentum.com.cn>
2018-02-06 09:00:55 +0800

    add the page four

--- src/basic/index/index.js
+++ src/basic/index/index.js
@@ -23,4 +23,19 @@ $(function() {
 
   init();
   // $('[data-toggle="tooltip"]').tooltip();
+
+  var swiper1 = new Swiper('.swiper-container.schools', {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: true
+  });
+  var swiper2 = new Swiper('.swiper-container.cities', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: true
+  });
 });
