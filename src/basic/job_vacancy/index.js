23c4eb574c4822c9708c18b6e7b624f6649f1676
ike1024 <ike.qian42@gmail.com>
2018-02-20 18:19:14 +1000

    Add files via upload

---
+++ src/basic/job_vacancy/index.js
@@ -0,0 +1,49 @@
+$(function() {
+
+  function initFullPage() {
+    var $fullPage = $('#fullpage');
+
+    if ($fullPage.length) {
+      var options = {
+        resize: true,
+        paddingTop: '70px',
+        responsiveWidth: 1200,
+        slidesNavigation: true
+      };
+
+      $fullPage.fullpage(options);
+    }
+  }
+
+  function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+      center: new google.maps.LatLng(54.976488, -1.606866),
+      zoom: 10
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+  }
+
+  function init() {
+    initFullPage();
+    myMap();
+  }
+
+  init();
+  // $('[data-toggle="tooltip"]').tooltip();
+
+  var swiper1 = new Swiper('.swiper-container.schools', {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: true
+  });
+  var swiper2 = new Swiper('.swiper-container.cities', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: true
+  });
+});
